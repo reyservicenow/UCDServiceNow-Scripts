f495b40419ed5a32b2ba4c6231101f6e72fba204
Earl Duque <eduque@ucdavis.edu>
2017-11-17 13:03:30 -0800

    making a gr for the script. need to do ajax

--- Request Items/Employee Recognition SPOT Award/Client Catalog Scripts/update nominee information.js
+++ Request Items/Employee Recognition SPOT Award/Client Catalog Scripts/update nominee information.js
@@ -3,6 +3,28 @@ function onChange(control, oldValue, newValue, isLoading) {
 		return;
 	}
 
-	//Type appropriate comment here, and begin script below
-
+	//split up list collector and then display the values in a text field
+	var fieldText = '';
+	var collectorResults = newValue;
+	var array = collectorResults.split(',');
+	
+	for (var i = 0; i < array.length; i++) {
+		var newGR = new GlideRecord('sys_user');
+		newGR.addQuery('sys_id', array[i]);
+		newGR.query();
+		if (newGR.next()) {
+			fieldText += newGR.name + ': ';
+			if (newGR.department) {
+				fieldText += newGR.department + ' ';
+			}
+			if (newGR.u_affiliation) {
+				fieldText += newGR.u_affiliation + ' ';
+			}
+			if (newGR.cost_center) {
+				fieldText += newGR.cost_center;
+			}
+			fieldText += '\n';
+		}
+	}
+	g_form.setValue('u_nominee_information', fieldText);
 }
