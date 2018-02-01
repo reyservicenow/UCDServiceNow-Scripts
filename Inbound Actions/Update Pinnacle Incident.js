ae6ab67c2b2a48f28513b19d175f2f9056752c0d
Earl Duque <eduque@ucdavis.edu>
2018-02-01 11:22:10 -0800

    clean up comments

--- Inbound Actions/Update Pinnacle Incident.js
+++ Inbound Actions/Update Pinnacle Incident.js
@@ -1,5 +1,6 @@
 (function runAction(/*GlideRecord*/ current, /*GlideRecord*/ event, /*EmailWrapper*/ email, /*ScopedEmailLogger*/ logger, /*EmailClassifier*/ classifier) {
 
+    //variable declarations
     var emailRaw = email.body_text.replace(/<(?:.|\n)*?>/gm, '\n');
 	emailRaw = emailRaw.replace(/&nbsp;/g,'');
 	var emailBody = emailRaw.toString().split("\n");
@@ -10,27 +11,36 @@
     var workOrderNumber;
     var closeIncident = false;
 
-    parseEmail(); //finds the sys_id for the desired incident
+    parseEmail(); //parses the email to find relevant information
 	
 	gs.log("Update Pinnacle Incident inbound action is updating:\nRunning Update Pinnacle Incident\n\n" + emailRaw);
 
-    var newGR = new GlideRecord('incident'); //selects the desired incident
+    var newGR = new GlideRecord('incident'); //finds the desired incident in ServiceNow
     newGR.addQuery('sys_id', incID);
-    newGR.addQuery('state', '!=', '7'); //do nothing if closed
+    newGR.addQuery('state', '!=', '7'); //do nothing to the servicenow ticket if closed
     newGR.query();
 
 	gs.log("Update Pinnacle Incident inbound action is updating:\nRecords found: " + newGR.getRowCount() + "\nRef: " + incID);
 	
     if (newGR.next()) {
-        constructComment(); //builds the comment
+        constructComment(); //builds the work note or comment
 
+        //if in the email it said Pinnacle Pre-Order Incident then the note goes to work notes, if it says Pinnacle Incident has been Closed: then the note goes to comments (customer facing)
         if (closeIncident == false) {
             newGR.work_notes += finalComment;
         } else if (closeIncident == true) {
             newGR.comments += finalComment;
         }
-        //email.subject + "\n\n" + email.body_text; //adds the email to the incident's work notes
+
+        //if in the email it said Pinnacle Incident has been Closed: then then update the ServiceNow incident state to resolved and the ServiceNow resolution code to Solved Remotely (Permanently)
+        if (closeIncident == true) { 
+            newGR.state = 5;
+            newGR.close_code = 'Solved Remotely (Permanently)';
+        }
+
         newGR.update();
+
+        //update the serviceNow email table
         sys_email.target_table = 'incident';
         sys_email.instance = newGR.sys_id;
 
@@ -39,31 +49,30 @@
 
     function parseEmail() {
         for (var i = 0; i < emailBody.length; i++) {
-            if (emailBody[i].indexOf("inc_ref_sys_id:") >= 0) { //look for "inc_ref_sys_id: xxxxxxxxxxxxx"
+            if (emailBody[i].indexOf("inc_ref_sys_id:") >= 0) { //look for "inc_ref_sys_id:"
                 incID = emailBody[i].split("inc_ref_sys_id:")[1].trim().slice(0, 32); //grab sys_id and make clip it to 32 chars
             } else if (emailBody[i].indexOf("Created By:") >= 0) { //look for created by
                 createdBy = emailBody[i].split("Created By:")[1].trim();
-            } else if (emailBody[i].indexOf("Pinnacle Pre-Order Incident:") >= 0) { //look for pre-order #
+            } else if (emailBody[i].indexOf("Pinnacle Pre-Order Incident:") >= 0) { //look for Pinnacle Pre-Order Incident
                 preOrderNumber = emailBody[i].split("Pinnacle Pre-Order Incident:")[1].trim(); 
             } else if (emailBody[i].indexOf("Pinnacle Incident:") >= 0) { //look for work order number
                 workOrderNumber = emailBody[i].split("Pinnacle Incident:")[1].trim(); 
-            } else if (emailBody[i].indexOf("Pinnacle Incident has been Closed:") >= 0) {
+            } else if (emailBody[i].indexOf("Pinnacle Incident has been Closed:") >= 0) { //look for Pinnacle Incident has been Closed: to see if this is a closure notification
                 closeIncident = true;
-                workOrderNumber = emailBody[i].split("Pinnacle Incident has been Closed:")[1].trim();
             }
         }
     }
 
     function constructComment() {
         var now = new GlideDateTime();
-        finalComment = "Message received from Pinnacle on " + now + " from " + createdBy + "\n";
+        finalComment = "Message received from Pinnacle on " + now + " from " + createdBy + "\n"; //comment template from Brent
         finalComment += "Incident number " + newGR.number;
         if (preOrderNumber) {
             finalComment += ", pre-order number " + preOrderNumber;
         } else if (workOrderNumber) {
             finalComment += ", work order number " + workOrderNumber;
         }
-        finalComment += "\nMessage:\n" + emailRaw;
+        finalComment += "\n" + emailRaw; //post the entire email body
     }
 
 })(current, event, email, logger, classifier); 
