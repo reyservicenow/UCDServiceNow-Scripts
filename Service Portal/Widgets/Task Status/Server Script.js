d4182064d1dcc5d6f31c5da0dc5f33aedf6a688e
Earl Duque <eduque@ucdavis.edu>
2018-07-17 11:15:02 -0700

    added comments, cleaned up html, and add a border bottom to the css

--- Service Portal/Widgets/Task Status/Server Script.js
+++ Service Portal/Widgets/Task Status/Server Script.js
@@ -1,21 +1,22 @@
 (function () {
-    /* populate the 'data' object */
-    /* e.g., data.table = $sp.getValue('table'); */
+    //get table from widget options
     var table_namegr = new GlideRecord('sys_db_object');
     table_namegr.get(options.table);
     var table_name = table_namegr.name.toString();
 
-
+    //retrieve records according to the widget options query
     var gr = new GlideRecord(table_name);
     gr.addEncodedQuery(options.encoded_query);
     gr.orderByDesc('number');
     gr.query();
     data.option1 = gr.getRowCount(); //temp
 
+    //declare objects
     data.records = [];
     data.state_array = [];
     data.state_columns = [];
 
+    //for each record, get its information
     while (gr.next()) {
         var record = {};
         record.number = gr.number + '';
@@ -23,14 +24,17 @@
         record.state = gr.state + '';
         record.state_value = gr.state.getDisplayValue() + '';
 
+        //if the state is a new state we've seen, add it to the table builder
         if (data.state_array.indexOf(gr.state.toString()) == -1) {
             data.state_array.push(gr.state.toString());
             data.state_array.sort();
         }
 
+        //add the record to the data object for display
         data.records.push(record);
     }
 
+    //figure out the column state names
     for (var i = 0; i < data.state_array.length; i++) {
         var columnsgr = new GlideRecord('sys_choice');
         columnsgr.addQuery('name', table_name);
@@ -40,11 +44,5 @@
         if (columnsgr.next()) data.state_columns.push(columnsgr.label + '');
     }
 
-    data.option2 = data.records.length; //temp
-    data.option3 = data.state_array.toString(); //temp
-    data.option4 = data.state_columns.toString(); //temp
-    data.option5 = options.table; //temp
-    data.option6 = data.state_columns.length; //temp
-    data.option7 = table_name;
 
 })();
